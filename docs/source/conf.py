09bce776f9275b71a4028e5c59103e45d81ebed6
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-06-06 16:51:11 -0500

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Giovanni Salinas'
 author = 'Giovanni Salinas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
